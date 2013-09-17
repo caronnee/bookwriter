8d3382af3b88a066713f66d5d942e39fd1f0fa02
caronnee <caronnee@12d61c89-9e2f-0a20-4022-5af21df91a08>
2013-09-17 18:26:20 +0000

    Testing richboxes

---
+++ trunk/BookWriter/BookViewer.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Minis
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckSize(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void checkSize(object sender, TextCompositionEventArgs e)
+        {
+            RichTextBox box = sender as RichTextBox;
+            double diff = box.ActualHeight - box.ExtentHeight;
+            if (diff < 0)
+            {
+                Keyboard.Focus(RightBox);
+                //RightBox.Focus();
+            }
+        }
+    }
+}
