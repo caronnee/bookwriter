4bd7eeb892f74f77e25f5b76280caafecc46b1a9
caronnee <caronnee@12d61c89-9e2f-0a20-4022-5af21df91a08>
2013-10-05 15:13:35 +0000

    Improved paging

--- trunk/BookWriter/BookViewer.xaml.cs
+++ trunk/BookWriter/BookViewer.xaml.cs
@@ -18,74 +18,63 @@ namespace MyBook
     /// </summary>
     public partial class BookViewPort : Window
     {
-        private String next;
-        private String previous;
+        PagesDone left;
+        PagesDone right;
 
         public BookViewPort()
         {
+            left = new PagesDone(); // TODO load
+            right = new PagesDone();//incompleted pages
             InitializeComponent();
         }
-        
-        private void CheckLines(object sender, TextChangedEventArgs e)
+        public PagesDone LeftSource
         {
-            TextBox s = sender as TextBox;
-            if (s.LineCount > s.MaxLines)
+            get
             {
-                int caretPosition = s.CaretIndex;
-                // get content of the next lines that are not visible
-                int index = s.GetLastVisibleLineIndex() + 1;
-                String rest = s.GetLineText(index);
-                s.Text = s.Text.Substring(0, s.Text.Length - rest.Length);
-                RightBox.Text = rest + RightBox.Text;
-                if (caretPosition >= s.Text.Length)
-                {
-                    // if the caret was at position that was cut
-                    RightBox.Focus(); // set caret position appropriately, after the word inserted
-                    RightBox.CaretIndex = rest.Length;
-                }
-                else
-                {
-                    s.CaretIndex = caretPosition;
-                }
-            }
-            else
-            {
-                // only if total length was removed
+                return left;
             }
         }
 
-        private void CheckForward(object sender, TextChangedEventArgs e)
+        public PagesDone RightSource
         {
-            TextBox box = sender as TextBox;
-            if (box.LineCount > box.MaxLines)
+            get
             {
-                int caretPosition = box.CaretIndex;
-                String added = "";
-                // only remove  the last few lines. Rest will be saved in data
-                for (int i = box.MaxLines; i < box.LineCount; i++)
-                {
-                    String str = box.GetLineText(i);
-                    added += str;
-                }
-                box.Text = box.Text.Substring(0, box.Text.Length - added.Length);
-                // only if caret was at the last place, move to the first box and fill everything possible
-                if (caretPosition >= added.Length)
-                {
-                    LeftBox.Focus();
-                    LeftBox.Text = added;
-                    LeftBox.CaretIndex = added.Length;
-                    RightBox.Text = ""; // clear the rightbox text
-                }
-                else
-                {
-                    box.CaretIndex = caretPosition;
-                }
-            }
-            else
-            {
-                //TODO
-                // check if you can add something from forward
+                return right;
             }
         }
+
+        //private void CheckLines(object sender, TextChangedEventArgs e)
+        //{
+        //     s = sender as TextBox;
+        //    if (s.LineCount > s.MaxLines)
+        //    {
+        //        int caretPosition = s.CaretIndex;
+        //        // get content of the next lines that are not visible
+        //        int index = s.GetLastVisibleLineIndex() + 1;
+        //        String rest = s.GetLineText(index);
+        //        s.Text = s.Text.Substring(0, s.Text.Length - rest.Length);
+        //        RightBox.Text = rest + RightBox.Text;
+        //        if (caretPosition >= s.Text.Length)
+        //        {
+        //            // if the caret was at position that was cut
+        //            RightBox.Focus(); // set caret position appropriately, after the word inserted
+        //            RightBox.CaretIndex = rest.Length;
+        //        }
+        //        else
+        //        {
+        //            s.CaretIndex = caretPosition;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        // only if total length was removed
+        //    }
+        //}
+
+        //private void CheckForward(object sender, TextChangedEventArgs e)
+        //{
+        //    PageCache box = sender as PageCache;
+        //    box.CheckSize();
+        //}
     }
 }
--- trunk/BookWriter/LimitedTextBox.cs
+++ trunk/BookWriter/LimitedTextBox.cs
@@ -8,25 +8,189 @@ using System.Drawing;
 using System.Windows.Media;
 using System.Globalization;
 using System.Windows;
+using System.ComponentModel;
 
 namespace MyBook
 {
-    class LimitedTextBox : TextBox
+    public enum PageMove
     {
+        PageLeft,
+        PageRight
+    };
+
+    public interface PageCache
+    {
+        void Append(String str);
+        void Prepend(String str);
+        void CheckSize();
+        void MakeActive(String str);
+    }
+
+    public class LimitedTextBox : TextBox, PageCache
+    {
+        public static readonly DependencyProperty NextProperty =
+    DependencyProperty.Register(
+    "Next", typeof(PageCache), typeof(LimitedTextBox));
+
+        public static readonly DependencyProperty PrevProperty =
+    DependencyProperty.Register(
+    "Prev", typeof(PageCache), typeof(LimitedTextBox));
+
+        public PageCache Next
+        {
+            get
+            {
+                return (PageCache)GetValue(NextProperty);
+            }
+            set
+            {
+                SetValue(NextProperty, value);
+            }
+        }
+
+        public PageCache Prev
+        {
+            get
+            {
+                return (PageCache)GetValue(PrevProperty);
+            }
+            set
+            {
+                SetValue(PrevProperty, value);
+            }
+        }
+
         public LimitedTextBox() 
         {
         }
-        private System.Windows.Size MeasureString(string candidate)
-        {
-            var formattedText = new FormattedText(
-                candidate,
-                CultureInfo.CurrentUICulture,
-                FlowDirection.LeftToRight,
-                new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch),
-                this.FontSize,
-                System.Windows.Media.Brushes.Black);
 
-            return new System.Windows.Size(formattedText.Width, formattedText.Height);
+        public virtual void MakeActive(String str)
+        {
+            Focus();
+            CaretIndex = str.Length;
+        }
+
+        public virtual void CheckSize()
+        {
+            if ( LineCount > MaxLines)
+            {
+                int caretPosition = CaretIndex;
+                String added = "";
+                // only remove the last few lines. Rest will be saved in data
+                for (int i = MaxLines; i < LineCount; i++)
+                {
+                    String str = GetLineText(i);
+                    added += str;
+                }
+                Text = Text.Substring(0, Text.Length - added.Length);
+                // only if caret was at the last place, move to the first box and fill everything possible
+                Next.Prepend(added);
+                if (caretPosition >= added.Length)
+                {
+                    Next.MakeActive(added);
+                }
+                else
+                {
+                    CaretIndex = caretPosition;
+                }
+            }
+            else
+            {
+                // System.Diagnostics.Debug.Assert(false);
+                // TODO
+                // check if you can add something from forward
+            }
+        }
+
+        public virtual void Append(String str)
+        {
+            // append to the last text
+            Text += str;
+        }
+
+        public virtual void Prepend(String str)
+        {
+            Text = str + Text;
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            CheckSize();
+        }
+        //private System.Windows.Size MeasureString(string candidate)
+        //{
+        //    var formattedText = new FormattedText(
+        //        candidate,
+        //        CultureInfo.CurrentUICulture,
+        //        FlowDirection.LeftToRight,
+        //        new Typeface(this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch),
+        //        this.FontSize,
+        //        System.Windows.Media.Brushes.Black);
+
+        //    return new System.Windows.Size(formattedText.Width, formattedText.Height);
+        //}
+    }
+
+    public class LimitedShowBox : LimitedTextBox
+    {
+        // any method that takes a string as the parameter and returns no value
+        public delegate void MoveHandler(PageMove page);
+
+        // Define an Event based on the above Delegate
+        public event MoveHandler moveHandler;
+
+        public LimitedShowBox()
+        {
+        }
+
+        // we have prev and next
+        public override void MakeActive(String str)
+        {
+            if (moveHandler == null)
+                return;
+            if (Next == null)
+            {
+                moveHandler(PageMove.PageLeft);
+            }
+            else if (Prev == null)
+            {
+                moveHandler(PageMove.PageRight);
+            }
+            else
+            {
+                throw new Exception("Not Implemented");
+            }
+        }
+    }
+
+    public class PagesDone : PageCache
+    {
+        String pagesContent;
+
+        public PagesDone()
+        {
+            pagesContent = "";
+        }
+        public void Load(String book)
+        {
+        }
+        public void Append(String str)
+        {
+            pagesContent += str;
+        }
+        public void Prepend(String str)
+        {
+            pagesContent = str + pagesContent;
+        }
+        public void CheckSize()
+        {
+            // everything is OK, no need to move
+        }
+        public void MakeActive(String str)
+        {
+            // should never get here, and do nothing
+            System.Diagnostics.Debug.Assert(false);
         }
     }
 }
