309e3c4eec358fc73734d96ec0d012155d1e7572
caronnee <caronnee@12d61c89-9e2f-0a20-4022-5af21df91a08>
2013-11-01 22:49:09 +0000

    Added mirror page

---
+++ trunk/BookWriter/HalfConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace MyBook
+{
+    class HalfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+        object parameter, CultureInfo culture)
+        {
+            Double val  = (Double) value;
+            return val/2;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            Double d = (Double)value;
+            return d * 2;
+        }
+    }
+}
