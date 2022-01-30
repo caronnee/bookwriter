bf6f45df59860b568c70ff239d7ef5673871eca1
cara <caronnee@gmail.com>
2022-01-30 21:16:49 +0100

    cleanup

--- BookWriter/Options/Settings.cs
+++ BookWriter/Options/Settings.cs
@@ -16,18 +16,6 @@ namespace MyBook
       set;
     }
 
-    public static int LineCount
-    {
-      get;
-      set;
-    }
-
-    public static int TextSizeCoef
-    {
-      get;
-      set;
-    }
-
     public static String BooksFolder
     {
       get;
@@ -52,7 +40,6 @@ namespace MyBook
       string str = GetName();
       System.IO.StreamWriter file = new System.IO.StreamWriter(str);
       file.WriteLine("booksfolder={0}", BooksFolder);
-      file.WriteLine("LineCount={0}", LineCount);
       file.Close();
     }
 
@@ -60,12 +47,9 @@ namespace MyBook
     {
       TempFolder = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
       TempFolder += "\\temp\\";
-
       Author = "Eva Pešková";
       // Default initialization
       BooksFolder = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Books";
-      TextSizeCoef = 1;
-      LineCount = 3;
 
       // load from the xml file
       string dir = GetName();
@@ -82,20 +66,6 @@ namespace MyBook
           if (split[0] == "Booksfolder")
           {
             BooksFolder = split[1];
-            continue;
-          }
-          if (split[0] == "LineCount")
-          {
-            int o;
-            if (int.TryParse(split[1], out o) == true)
-              LineCount = o;
-            continue;
-          }
-          if ( split[0] == "textcoef" )
-          {
-            int o;
-            if (int.TryParse(split[1],out o) == true)
-              TextSizeCoef = o;
             continue;
           }
         }
